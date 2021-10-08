16ab53a42b414cb8f04dd668e0ad41dde50febc9
faysaljafry <faysaljafryofficial@gmail.com>
2021-10-08 20:27:05 +0500

    App Comp Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,23 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class AppComponent {
   //Get Existing data from local Storage
-  dataBase: any = localStorage.getItem("Records");
-  parsed: any = JSON.parse(this.dataBase)
+  dataBase: any = localStorage.getItem('Records');
+  parsed: any = JSON.parse(this.dataBase);
   index: number = 0;
   currentRecord: any = this.parsed;
   // Blood group Options
-  public options: string[] = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
-  selected = "A+";
-  selectedQuantity = "10";
+  public options: string[] = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+  selected = 'A+';
+  selectedQuantity = '10';
   ProfileData: any = {};
   addData(form: NgForm) {
     this.ProfileData = form.value;
     var found = false;
-    for(var i = 0; i < this.currentRecord.length; i++){
-      if(this.currentRecord[i].contact == form.value.contact)
-        found = true
+    for (var i = 0; i < this.currentRecord.length; i++) {
+      if (this.currentRecord[i].contact == form.value.contact) found = true;
     }
-    if(!found)
-      this.currentRecord.push(this.ProfileData)
-    localStorage.setItem("Records", JSON.stringify(this.currentRecord))
-    
+    if (!found) this.currentRecord.push(this.ProfileData);
+    localStorage.setItem('Records', JSON.stringify(this.currentRecord));
   }
   title = 'CUBE Blood Bank';
 }
