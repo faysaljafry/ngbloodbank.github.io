789150e1a7eb4ca35aa716e9db167bf773ba90c1
faysaljafry <faysaljafryofficial@gmail.com>
2021-10-08 19:17:54 +0500

    Blood_Bank app using simple ngModel Approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-blood-bank';
+  //Get Existing data from local Storage
+  dataBase: any = localStorage.getItem("Records");
+  parsed: any = JSON.parse(this.dataBase)
+  index: number = 0;
+  currentRecord: any = this.parsed;
+  // Blood group Options
+  public options: string[] = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
+  selected = "A+";
+  selectedQuantity = "10";
+  ProfileData: any = {};
+  addData(form: NgForm) {
+    this.ProfileData = form.value;
+    var found = false;
+    for(var i = 0; i < this.currentRecord.length; i++){
+      if(this.currentRecord[i].contact == form.value.contact)
+        found = true
+    }
+    if(!found)
+      this.currentRecord.push(this.ProfileData)
+    localStorage.setItem("Records", JSON.stringify(this.currentRecord))
+    
+  }
+  title = 'CUBE Blood Bank';
 }
